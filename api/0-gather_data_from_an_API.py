39d7b52abf673a5a964222b872fa14b359216d53
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-24 01:01:49 -0800

    errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,9 +2,9 @@
 """
    python script that returns TODOlist progress for a given employee ID
 """
-import json 
+import json
 import requests
-from sys import argv 
+from sys import argv
 
 
 if __name__ == "__main__":
@@ -14,29 +14,29 @@ if __name__ == "__main__":
     request_employee = requests.get(
          'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
     """
-    convert json to dictionary 
+    convert json to dictionary
     """
     employee = json.loads(request_employee.text)
     """
-    extract employee name 
+    extract employee name
     """
     employee_name = employee.get("name")
 
     """
     request user's TODO list
     """
-    request_todos =requests.get(
+    request_todos = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
     """
     dictionary to store task status in boolean format
-    """    
+    """
     tasks = {}
     """
     convert json to list of dictionaries
     """
     employee_todos = json.loads(request_todos.text)
     """
-    loop through dictionary & get completed tasks 
+    loop through dictionary & get completed tasks
     """
     for dictionary in employee_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
@@ -52,4 +52,3 @@ if __name__ == "__main__":
         for k, v in tasks.items():
             if v is True:
                 print("\t{}".format(k))
-
