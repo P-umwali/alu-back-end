def35e1cfb2725ba8c398059b85ae91bc7b0d336
Pamela UMWALI <p.umwali@alustudent.com>
2023-02-24 17:19:59 +0200

    pre last task

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+    Module
+"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+         'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+    convert json to dictionary 
+    """
+    user = json.loads(request_employee.text)
+    """
+    extract username
+    """
+    username = user.get("username")
+    
+    """
+        request user's TO DO list
+    """
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    """
+        dictionary to store task status(completed)in boolean format
+    """
+    tasks = {}
+    """
+        convert json to list of dictionaries
+    """
+    user_todos = json.loads(request_todos.text)
+    """
+        loop through dictionary and get completed task
+    """
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+        task_list = []
+    for k, v in tasks.items():
+        task_list.append({
+            "task": k,
+            "completed": v,
+            "username": username
+        })
+
+    json_to_dump = {argv[1]: task_list}
+    """
+        export to JSON
+    """
+    with open('{}.json'.format(argv[1]), mode='w') as file:
+        json.dump(json_to_dump, file)
